307f1ff430462ee1d274b77d0e35ed48dfb9ae3d
Tanji <tanji3000@gmail.com>
2019-05-14 08:11:27 +0900

    refresh

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
-source 'https://rubygems.org'
-gem 'jekyll'
-gem 'jekyll-archives'
-gem 'jekyll-sitemap'
-gem 'jekyll-paginate'
-gem 'youtube'
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+gemspec
--- _posts/rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in ./2016-10-09-dropbox_github.markdown ./2015-07-10-Linux_Zip.markdown ./2015-10-21-cmake-install.markdown ./2015-05-28-Linux-Epelerror.markdown ./2015-06-08-vim_window_split.markdown;do
-
-    sed -i /categories/s/Linux/"리눅스_기술문서"/g $i
-
-done
-
